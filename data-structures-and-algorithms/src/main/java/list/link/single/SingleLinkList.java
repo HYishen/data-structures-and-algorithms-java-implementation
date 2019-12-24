8807bbdc900520db9e2123dfcb549307ff501d9c
Yishen <844147804@qq.com>
2019-12-24 18:43:41 +0800

    Commit single link list demo.

---
+++ data-structures-and-algorithms/src/main/java/list/link/single/SingleLinkList.java
@@ -0,0 +1,90 @@
+package list.link.single;
+
+import java.util.Objects;
+
+/**
+ * <pre>
+ * Single Link List。
+ * </pre>
+ *
+ * @author Yishen 844147804@qq.com
+ * @version 1.00.00
+ */
+public class SingleLinkList<T> {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public SingleLinkList() {
+        this.head = null;
+        this.tail = null;
+        size = 0;
+    }
+
+    public void add(T t) {
+        if (this.head == null) {
+            head = new Node(t);
+            tail = head;
+        } else {
+            tail.next = new Node(t);
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    public boolean remove(T t) {
+        if (head == null) {
+            return false;
+        }
+        if (Objects.equals(head.data, t)) {
+            head = head.next;
+            size--;
+            return true;
+        }
+
+        Node preNode = head;
+        Node currentNode = head.next;
+        while (currentNode != null) {
+            if (Objects.equals(currentNode.data, t)) {
+                preNode.next = currentNode.next;
+                size--;
+                return true;
+            }
+            preNode = currentNode;
+            currentNode = currentNode.next;
+        }
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        Node currentNode = head;
+        while (currentNode != null) {
+            sb.append(currentNode.data.toString() + ",");
+            currentNode = currentNode.next;
+        }
+        if (sb.length() > 1) {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    private class Node {
+        private T data;
+        private Node next;
+
+        public Node() {
+        }
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+}
---
+++ data-structures-and-algorithms/src/test/java/list/link/single/TestSingleLinkList.java
@@ -0,0 +1,47 @@
+package list.link.single;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * <pre>
+ * Test Single Link List。
+ * </pre>
+ *
+ * @author Yishen 844147804@qq.com
+ * @version 1.00.00
+ */
+public class TestSingleLinkList {
+    @Test
+    public void testSingleLinkListAdd() {
+        SingleLinkList<String> sll = new SingleLinkList<>();
+        sll.add("a");
+        sll.add("b");
+        sll.add("c");
+        sll.add("d");
+        Assertions.assertEquals("[a,b,c,d]", sll.toString());
+        Assertions.assertEquals(4, sll.size());
+        sll.remove("c");
+        Assertions.assertEquals("[a,b,d]", sll.toString());
+        Assertions.assertEquals(3, sll.size());
+    }
+
+    @Test
+    public void testSingleLinkListRemoveSingle() {
+        SingleLinkList<String> sll = new SingleLinkList<>();
+        sll.add("a");
+        Assertions.assertEquals("[a]", sll.toString());
+        Assertions.assertEquals(1, sll.size());
+        sll.remove("a");
+        Assertions.assertEquals("[]", sll.toString());
+        Assertions.assertEquals(0, sll.size());
+    }
+
+    @Test
+    public void testSingleLinkListRemoveEmpty() {
+        SingleLinkList<String> sll = new SingleLinkList<>();
+        sll.remove("a");
+        Assertions.assertEquals("[]", sll.toString());
+        Assertions.assertEquals(0, sll.size());
+    }
+}
